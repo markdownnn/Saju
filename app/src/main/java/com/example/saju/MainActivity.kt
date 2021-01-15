fd584c99d80616aaee4333b849db4559d46b4173
Hyeok <mh960110@gmail.com>
2021-01-16 00:56:35 +0900

    Enable binding and add picker buttons

--- app/src/main/java/com/example/saju/MainActivity.kt
+++ app/src/main/java/com/example/saju/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.saju
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.saju.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var timePicker: TimePickerHelper
+    lateinit var datePicker: DatePickerHelper
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        timePicker = TimePickerHelper(this, false, true)
+        binding.btSelectTime.setOnClickListener {
+            showTimePickerDialog()
+        }
+        datePicker = DatePickerHelper(this, true)
+        binding.btSelectDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val cal = Calendar.getInstance()
+        val h = cal.get(Calendar.HOUR_OF_DAY)
+        val m = cal.get(Calendar.MINUTE)
+        timePicker.showDialog(h, m, object : TimePickerHelper.Callback {
+            override fun onTimeSelected(hourOfDay: Int, minute: Int) {
+                val hourStr = if (hourOfDay < 10) "0${hourOfDay}" else "${hourOfDay}"
+                val minuteStr = if (minute < 10) "0${minute}" else "${minute}"
+                binding.tvTime.text = "${hourStr}:${minuteStr}"
+            }
+        })
+    }
+
+    private fun showDatePickerDialog() {
+        val cal = Calendar.getInstance()
+        val d = cal.get(Calendar.DAY_OF_MONTH)
+        val m = cal.get(Calendar.MONTH)
+        val y = cal.get(Calendar.YEAR)
+        datePicker.showDialog(d, m, y, object : DatePickerHelper.Callback {
+            override fun onDateSelected(dayofMonth: Int, month: Int, year: Int) {
+                val dayStr = if (dayofMonth < 10) "0${dayofMonth}" else "${dayofMonth}"
+                val mon = month + 1
+                val monthStr = if (mon < 10) "0${mon}" else "${mon}"
+                binding.tvDate.text = "${dayStr}-${monthStr}-${year}"
+            }
+        })
     }
 }
